b952751592a010b8c035870f649184f6cde1883b
Dano <danielaherniv@gmail.com>
2020-06-05 20:51:39 -0400

    updating length of description

--- src/core/Card.js
+++ src/core/Card.js
@@ -111,7 +111,7 @@ const Card = ({
       <div className='card-body'>
         {shouldRedirect(redirect)}
         <ShowImage item={product} url='product' />
-        <p className='lead mt-2'>{product.description.substring(0, 200)}</p>
+        <p className='lead mt-2'>{product.description.substring(0, 500)}</p>
         <p className='black-10'>${product.price}</p>
         <p className='black-9'>
           Category: {product.category && product.category.name}
